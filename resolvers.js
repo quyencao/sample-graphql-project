86ff0befb566a72ad053d95b8e353383a0ae6254
quyencao <boy20195@gmail.com>
2019-10-21 16:29:41 +0700

    new commit

--- resolvers.js
+++ resolvers.js
@@ -3,24 +3,31 @@ const db = require('./db');
 const resolver = {
     Query: {
         getTodo: (_, args) => {
-            return db.getTable("todosTable").getRecordById(args.id).then(todo => {
-                return todo.Item
+            return db.getTable("todosTable").getRecordById(args.id).then(data => {
+                return data.Item
             }).catch(err => null);
         },
         getTodos: (_, args) => {
             return  db.getTable("todosTable").getRecords({}).then(data => {
                 return data.Items
             }).catch(err => null);
-        }
+        },
+        getUser: (_, args) => {
+            return db.getTable("usersTable").getRecordById(args.id).then(data => {
+                return data.Item
+            }).catch(err => null);
+        },
+        getUsers: (_, args) => {
+            return  db.getTable("usersTable").getRecords({}).then(data => {
+                return data.Items
+            }).catch(err => null);
+        },
     },
     Mutation: {
         createTodo: (_, args) => {
             return db.getTable("todosTable")
-            .insertRecord({
-                text: args.text,
-                completed: false
-            })
-            .then(todo => todo)
+            .insertRecord(args.input)
+            .then(data => data)
             .catch(err => {
                 throw err;
             })
@@ -32,17 +39,31 @@ const resolver = {
                 .catch(err => false);
         },
         updateTodo: (_, args) => {
-            const new_data = {
-                text: args.text 
-            }
-            if ('completed' in args) {
-                new_data["completed"] = args.completed
-            }
             return db.getTable("todosTable")
-                .updateRecordById(args.id, new_data)
+                .updateRecordById(args.id, args.input)
                 .then(data => data.Attributes)
                 .catch(err => null);
-        }
+        },
+        createUser: (_, args) => {
+            return db.getTable("usersTable")
+            .insertRecord(args.input)
+            .then(data => data)
+            .catch(err => {
+                throw err;
+            })
+        },
+        deleteUser: (_, args) => {
+            return db.getTable("usersTable")
+                .deleteRecordById(args.id)
+                .then(data => true)
+                .catch(err => false);
+        },
+        updateUser: (_, args) => {
+            return db.getTable("usersTable")
+                .updateRecordById(args.id, args.input)
+                .then(data => data.Attributes)
+                .catch(err => null);
+        },
     }
 }
 
--- schema.js
+++ schema.js
@@ -1,21 +1,47 @@
 const { gql } = require("apollo-server-lambda");
 
 const typeDefs = gql`
-  type Todo {
-    id: ID!
-    text: String!
-    completed: Boolean!
+  type Mutation {
+    createTodo(input: TodoInput!): Todo
+    updateTodo(id: ID!, input: TodoInput!): Todo
+    deleteTodo(id: ID!): Boolean
+    createUser(input: UserInput!): User
+    updateUser(id: ID!, input: UserInput!): User
+    deleteUser(id: ID!): Boolean
   }
 
   type Query {
     getTodo(id: ID!): Todo
     getTodos: [Todo!]
+    getUser(id: ID!): User
+    getUsers: [User!]
   }
 
-  type Mutation {
-    createTodo(text: String!): Todo
-    deleteTodo(id: ID!): Boolean!
-    updateTodo(id: ID!, text: String!, completed: Boolean): Todo
+  input TodoInput {
+    text: String!
+    completed: Boolean
+  }
+
+  input UserInput {
+    username: String!
+    email: String!
+    desc: String
+    password: String!
+  }
+
+
+  type Todo {
+    id: ID!
+    text: String!
+    completed: Boolean
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    desc: String
+    password: String!
   }
 `;
 
