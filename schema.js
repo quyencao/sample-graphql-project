3ff8db1dcb35a7469384fc93888154452ef49677
quyencao <boy20195@gmail.com>
2019-10-22 11:09:55 +0700

    new commit

--- schema.js
+++ schema.js
@@ -1,60 +1,54 @@
 const { gql } = require("apollo-server-lambda");
 
 const typeDefs = gql`
-  type Mutation {
-    createTodo(input: CreateTodoInput!): Todo
-    updateTodo(id: ID!, input: UpdateTodoInput!): Todo
-    deleteTodo(id: ID!): Boolean
-    createUser(input: CreateUserInput!): User
-    updateUser(id: ID!, input: UpdateUserInput!): User
-    deleteUser(id: ID!): Boolean
-  }
+type Mutation {
+  createTodo(input: CreateTodoInput!): Todo
+  updateTodo(id: ID!, input: UpdateTodoInput!): Todo
+  deleteTodo(id: ID!): Boolean
+  createUser(input: CreateUserInput!): User
+  updateUser(id: ID!, input: UpdateUserInput!): User
+  deleteUser(id: ID!): Boolean
+}
 
-  type Query {
-    getTodo(id: ID!): Todo
-    getTodos: [Todo!]
-    getUser(id: ID!): User
-    getUsers: [User!]
-  }
+type Query {
+  getTodo(id: ID!): Todo
+  getTodos: [Todo!]
+  getUser(id: ID!): User
+  getUsers: [User!]
+}
 
-  input CreateTodoInput {
-    text: String!
-    completed: Boolean
-  }
+input CreateTodoInput {
+  text: String!
+  completed: Boolean
+}
 
-  input UpdateTodoInput {
-    text: String
-    completed: Boolean
-  }
+input UpdateTodoInput {
+  text: String
+  completed: Boolean
+}
+input CreateUserInput {
+  username: String!
+  email: String!
+  desc: String
+}
 
-  input CreateUserInput {
-    username: String!
-    email: String!
-    desc: String
-    password: String!
-  }
+input UpdateUserInput {
+  username: String
+  email: String
+  desc: String
+}
 
-  input UpdateUserInput {
-    username: String
-    email: String
-    desc: String
-    password: String
-  }
-
-
-  type Todo {
-    id: ID!
-    text: String!
-    completed: Boolean
-  }
-
-  type User {
-    id: ID!
-    username: String!
-    email: String!
-    desc: String
-    password: String!
-  }
+type Todo {
+  id: ID!
+  text: String!
+  completed: Boolean
+}
+type User {
+  id: ID!
+  username: String!
+  email: String!
+  desc: String
+}
 `;
 
 module.exports = typeDefs;
